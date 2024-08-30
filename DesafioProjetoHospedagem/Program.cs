adcfdb6dd50c138d96466fc6eb6bc9265a22e2e3
Eliomar Dourado Vieira <53126223+eliomardev@users.noreply.github.com>
2024-08-30 16:04:15 -0300

    Add files via upload
    Construção de um sistema de hospedagem, que será usado para realizar uma reserva em um hotel. Usei a classe Pessoa, que representa o hóspede, a classe Suíte, e a classe Reserva, que fará um relacionamento entre ambos. O programa calcula corretamente os valores dos métodos da classe Reserva, que precisará trazer a quantidade de hóspedes e o valor da diária, concedendo um desconto de 10% para caso a reserva seja para um período maior que 10 dias.
    
    Itens trabalhados:
    Lógica de Programação e POO;
    Conhecimentos Básico(.NET, Git, GitHub);
    Computador com SO de sua preferência(Windows, Linux, Mac OS);
    Ferramentas de Desenvolvimento (IDE)

---
+++ DesafioProjetoHospedagem/Program.cs
@@ -0,0 +1,25 @@
+﻿using DesafioProjetoHospedagem.Models;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+//Cria os modelos de hospedes e cadastra na lista de hospedes
+List<Pessoa> hospedes = new List<Pessoa>(); 
+
+Pessoa p1 = new Pessoa(nome: "Hospede 1");
+Pessoa p2 = new Pessoa(nome: "Hospede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+//Cria a suite
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+//Cria uma nova reserva, passando a suite e os hospedes
+Reserva reserva = new Reserva(diasReservados: 5);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+//Exibe a quantidade de hospedes e o valor da diaria
+Console.WriteLine($"Hospedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diaria: {reserva.CalcularValorDiaria()}");
